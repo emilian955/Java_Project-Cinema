f3a991af053a123b5991d3d4a86f75bc1ed9f73a
Theo3699 <samson.theodor3699@gmail.com>
2022-01-08 13:12:55 +0200

    JPA setup complete

---
+++ src/main/java/com/example/entities/ProjectionEntity.java
@@ -0,0 +1,87 @@
+package com.example.entities;
+
+import javax.persistence.*;
+
+@Table(name = "projections")
+@Entity
+public class ProjectionEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_projection", nullable = false)
+    private Integer id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "id_room", nullable = false)
+    private RoomEntity idRoom;
+
+    @Column(name = "movie_title", nullable = false)
+    private String movieTitle;
+
+    @Column(name = "start_time", nullable = false, length = 10)
+    private String startTime;
+
+    @Column(name = "duration", nullable = false, length = 10)
+    private String duration;
+
+    @Column(name = "available_places", nullable = false)
+    private Integer availablePlaces;
+
+    @Column(name = "poster_url", nullable = false)
+    private String posterUrl;
+
+    public String getPosterUrl() {
+        return posterUrl;
+    }
+
+    public void setPosterUrl(String posterUrl) {
+        this.posterUrl = posterUrl;
+    }
+
+    public Integer getAvailablePlaces() {
+        return availablePlaces;
+    }
+
+    public void setAvailablePlaces(Integer availablePlaces) {
+        this.availablePlaces = availablePlaces;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getMovieTitle() {
+        return movieTitle;
+    }
+
+    public void setMovieTitle(String movieTitle) {
+        this.movieTitle = movieTitle;
+    }
+
+    public RoomEntity getIdRoom() {
+        return idRoom;
+    }
+
+    public void setIdRoom(RoomEntity idRoom) {
+        this.idRoom = idRoom;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/com/example/entities/TicketEntity.java
@@ -0,0 +1,76 @@
+package com.example.entities;
+
+import javax.persistence.*;
+
+@Table(name = "tickets")
+@Entity
+public class TicketEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_ticket", nullable = false)
+    private Integer id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "id_projection", nullable = false)
+    private ProjectionEntity idProjection;
+
+    @Column(name = "no_of_row", nullable = false)
+    private Integer noOfRow;
+
+    @Column(name = "no_of_column", nullable = false)
+    private Integer noOfColumn;
+
+    @Column(name = "name_buyer", nullable = false)
+    private String nameBuyer;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNameBuyer() {
+        return nameBuyer;
+    }
+
+    public void setNameBuyer(String nameBuyer) {
+        this.nameBuyer = nameBuyer;
+    }
+
+    public Integer getNoOfColumn() {
+        return noOfColumn;
+    }
+
+    public void setNoOfColumn(Integer noOfColumn) {
+        this.noOfColumn = noOfColumn;
+    }
+
+    public Integer getNoOfRow() {
+        return noOfRow;
+    }
+
+    public void setNoOfRow(Integer noOfRow) {
+        this.noOfRow = noOfRow;
+    }
+
+    public ProjectionEntity getIdProjection() {
+        return idProjection;
+    }
+
+    public void setIdProjection(ProjectionEntity idProjection) {
+        this.idProjection = idProjection;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
