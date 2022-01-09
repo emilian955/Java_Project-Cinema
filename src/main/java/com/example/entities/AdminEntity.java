58de747d62b59a4fdd6993b207aeda20d530a481
andrei1428 <53914632+andrei1428@users.noreply.github.com>
2022-01-09 18:57:27 +0200

    jpa support for admin added

---
+++ src/main/java/com/example/entities/AdminEntity.java
@@ -0,0 +1,59 @@
+package com.example.entities;
+
+import javax.persistence.*;
+
+@Table(name = "admins")
+@Entity
+@NamedQueries({
+        @NamedQuery(name="Admins.getAll",
+                query="SELECT a FROM AdminEntity a"),
+        @NamedQuery(name="Admins.findByID",
+                query="SELECT a FROM AdminEntity a WHERE a.id = :id")
+})
+public class AdminEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_admin", nullable = false)
+    private Integer id;
+
+    @Column(name = "admin_name", nullable = false)
+    private String adminName;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAdminName() {
+        return adminName;
+    }
+
+    public void setAdminName(String adminName) {
+        this.adminName = adminName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/com/example/repositories/AdminRepository.java
@@ -0,0 +1,53 @@
+package com.example.repositories;
+
+import com.example.entities.AdminEntity;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class AdminRepository {
+
+    @Inject
+    protected EntityManager cinemaPU;
+
+    public AdminRepository() {
+    }
+
+    public void save(AdminRepository admin) {
+        cinemaPU.getTransaction().begin();
+        cinemaPU.persist(admin);
+        cinemaPU.getTransaction().commit();
+    }
+
+    public List<AdminEntity> getAll() {
+        Query query = cinemaPU.createNamedQuery("Admins.getAll");
+        return ((Collection<AdminEntity>) query.getResultList()).stream().collect(Collectors.toList());
+    }
+
+    public AdminEntity findByID(Integer id) {
+        Query query = cinemaPU.createNamedQuery("Admins.findByID");
+        //id is integer here, but method "setParameter" might take only Strings
+        query.setParameter("id", id);
+        Collection projectionResults = query.getResultList();
+        return (AdminEntity) projectionResults.iterator().next();
+    }
+
+    public boolean remove(Integer id) {
+        try {
+            Query query = cinemaPU.createNamedQuery("Admins.findByID");
+            query.setParameter("id", id);
+            AdminEntity entryToBeRemoved = findByID(id);
+            cinemaPU.getTransaction().begin();
+            cinemaPU.remove(entryToBeRemoved);
+            cinemaPU.getTransaction().commit();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
